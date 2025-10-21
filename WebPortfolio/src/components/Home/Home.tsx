e6dab50d2de3abb3484ef5b6ba6a4fe31fb29e4e
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-10-21 12:13:10 +0200

    Done with Home and banner

--- WebPortfolio/src/components/Home/Home.tsx
+++ WebPortfolio/src/components/Home/Home.tsx
@@ -1,27 +1,33 @@
 import { LearnMoreBtn } from "../learnMoreBtn";
 
 export const Home = () => {
+  const scrollToAbout = () => {
+    const aboutSection = document.getElementById("aboutMe");
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="HOME">
-      <div className="BANNER">
-
-          <div className="row ">
-            <div className="col-12 col-md-6 d-flex justify-content-center">
-              <img src="/images/ralisMain.png" alt="" className="BannerImage" />
-            </div>
-            <div className="col-12 col-md-6 d-flex flex-column justify-content-center text-start">
-              <h1 className="TITLE">
-                <span>Hi, I am </span> Hristian Ralevski
-              </h1>
-              <p className="description">
-                I’m interested in field of web development and creating a
-                dinamicand modern leyouts for web pages. Now I'm at Brainster
-                academy for Front-End .
-              </p>
-              <LearnMoreBtn targetId="aboutMe" />
-            </div>
-          </div>
-     
+      <div className="BANNER d-flex flex-column">
+        <div className="row justify-content-center align-items-center w-50 ">
+          <h5 className="subTitle">Hello I'm Hristijan</h5>
+          <h1 className="mainTitle mb-3">
+            The best thing come from living{" "}
+            <span className="quote">outside of your comfort zone.</span>
+          </h1>
+          <p className="mainDesc">
+            I'm an enthusiast in the field of web programming, passionate about
+            building dynamic and meaningful web applications. My journey began
+            with creating simple projects using HTML and CSS, and over time,
+            I've grown into developing fully functional, modern web pages that
+            bring real value to users.
+          </p>
+        </div>
+        <button className="goOnAboutMe" onClick={scrollToAbout}>
+          About Me
+        </button>
       </div>
     </div>
   );
